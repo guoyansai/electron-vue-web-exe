30e615bf540434c2336f64bf5699c9e08e17d607
guoyansai <guoyansai@qq.com>
2020-10-30 13:00:15 +0800

    first commit

---
+++ src/render/libs/interface/baseContext.js
@@ -0,0 +1,8 @@
+/**
+ * name: 上下文（页面、接口）
+*/
+const name = '/pageContext'
+const api = '/api';
+module.exports = {
+    name, api
+}
---
+++ src/render/views/home.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="content">
+        <h3>当前渲染进程版本号：{{version.version.join('.')}}</h3>
+        <h1>Welcome to electron-vue-web-exe!111</h1>
+        <h2>name:{{userInfo.name}}</h2>
+        <h2>address:{{userInfo.address}}</h2>
+        <button @click="changeAddress">设置address为tianjin</button>
+        <!-- <button @click="toOrderPage">去订单页</button> -->
+        <router-link to="order" tag="button">去订单页</router-link>
+    </div>
+</template>
+
+<script>
+import { mapState, mapMutations, mapActions } from "vuex";
+import version from '@config/version.js';
+export default {
+    data() {
+        return {
+            version
+        }
+    },
+    computed: {
+        ...mapState(["userInfo"])
+    },
+    methods: {
+        ...mapActions(['getUserInfo']),
+        ...mapMutations(['setUserInfo']),
+        changeAddress() {
+            this.setUserInfo({
+                address: "tianjin"
+            });
+        },
+        toOrderPage() {
+            this.$router.push({
+                name: "order"
+            });
+        }
+    }
+};
+</script>
+<style></style>
